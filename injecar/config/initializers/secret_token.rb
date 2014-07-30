745e203648dec07513273ce6112a67602d391a90
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-07-30 11:29:17 -0300

    Adicionado o refinery, configurado a template. 3h refs #1

---
+++ injecar/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Injecar::Application.config.secret_token = '84a787b36b282a4b33e67d4b0d0dd1a7bd21fa89200e372ecd829985b4806f23939e12a5050b7accc3348352c04338db8731a4661ef67ec0f15fa088b639bf19'
