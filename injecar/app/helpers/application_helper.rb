488a4f69fb978000ffffc989f0045c4c623fe7e7
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-07-31 23:57:46 -0300

    Adicionado conteúdos e estilzando as páginas. 6,20h refs #2

--- injecar/app/helpers/application_helper.rb
+++ injecar/app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def menu_refinery_top
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    presenter.css = "right"
+    presenter
+  end
+
+  def menu_refinery_footer
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+    p ">"*80
+    p presenter.inspect
+    p "*"*100
+    presenter.css = "home-social"
+    #presenter.ul_css = "home-social"
+    #presenter.li_css = cycle("fi-web", "fi-telephone", "fi-calendar", "fi-map")
+    presenter
+  end
 end
