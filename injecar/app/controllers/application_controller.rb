74265d3b24d732b5dcb12839bf44200aa6dfa942
Lucas Cardoso de Araujo <lucas.ca.de.araujo@gmail.com>
2014-08-04 14:45:00 -0300

    Criado o agendamento.

--- injecar/app/controllers/application_controller.rb
+++ injecar/app/controllers/application_controller.rb
@@ -1,14 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def mandar_email
+  def mandar_email_contato
     begin
     EnviarEmail.emitir_email(params["email"], params["conteudo"], params["assunto"]).deliver
-    flash[:notice] = "E-mail enviado, com sucesso!!"
+    flash[:notice] = "E-mail enviado, com sucesso!"
     rescue
     flash[:error] = "E-mail, inválido!"
     end
     redirect_to "/contato"
-
   end
+
+  def mandar_email_agendamento
+    begin
+    EnviarEmail.emitir_email(params["email"], params["conteudo"], params["assunto"]).deliver
+    flash[:notice] = "E-mail enviado, com sucesso!"
+    rescue
+    flash[:error] = "E-mail, inválido!"
+    end
+    redirect_to "/agendamento"
+  end
+
 end
--- injecar/config/routes.rb
+++ injecar/config/routes.rb
@@ -7,7 +7,8 @@ Injecar::Application.routes.draw do
   # We ask that you don't use the :as option here, as Refinery relies on it being the default of "refinery"
   mount Refinery::Core::Engine, :at => '/'
 
-  match "/mandar_email", to: "application#mandar_email"
+  match "/mandar_email", to: "application#mandar_email_contato"
+  match "/agendamento_email", to: "application#mandar_email_agendamento"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
